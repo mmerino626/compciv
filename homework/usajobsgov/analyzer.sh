d52b242941a1f40a6bd26c8481a1f6d71b52a847
mmerino626 <mmerino@corn23.stanford.edu>
2015-03-14 14:47:41 -0700

    USA Jobs commit

--- homework/usajobsgov/analyzer.sh
+++ homework/usajobsgov/analyzer.sh
@@ -3,8 +3,7 @@ yearly_jobs=$(cat $datadir/*.json | jq '.JobData[] | select(.SalaryBasis == "Per
 
 simple_rows=$(echo $yearly_jobs | jq '. | {JobTitle, SalaryMin, SalaryMax}')
 
-echo $simple_rows | jq -r '.JobTitle' | sort | uniq -c | sort -rn | head -n 25 | \
-while read -r line; do
+echo $simple_rows | jq -r '.JobTitle' | sort | uniq -c | sort -rn | head -n 25 | while read -r line; do
 	title=$(echo $line | grep -oE '[[:alpha:]].+')
 	filtered_rows=$(echo $simple_rows | jq "select(.JobTitle == \"$title\")")
 	min=$(echo $filtered_rows | jq -r '.SalaryMin' | tr -d '$' | tr -d ',' | sort -n | head -n 1)
